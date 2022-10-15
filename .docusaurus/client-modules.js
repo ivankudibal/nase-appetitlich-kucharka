475f8e0862aed3cfb9557df2d4da482e8d97fd85
Ivan Kudibal <8028110+ivankudibal@users.noreply.github.com>
2022-10-15 19:55:38 +0200

    chore: rm .docusaurus, add to gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/media/ivan/dilna/github.com/ivankudibal/nase-appetitlich-kucharka/node_modules/infima/dist/css/default/default.css'),
-  require('/media/ivan/dilna/github.com/ivankudibal/nase-appetitlich-kucharka/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/media/ivan/dilna/github.com/ivankudibal/nase-appetitlich-kucharka/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/media/ivan/dilna/github.com/ivankudibal/nase-appetitlich-kucharka/src/css/custom.css'),
-];
